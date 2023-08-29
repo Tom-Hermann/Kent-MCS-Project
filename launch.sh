55de7b1a9db745f483b5685f8189a0669217acc7
Tom Hermann <tom.hermann@epitech.eu>
2023-08-29 20:05:04 +0100

    [ADD] more model to compare

--- launch.sh
+++ launch.sh
@@ -8,6 +8,37 @@ benchmark=""
 install=""
 
 
+# Function to display script usage
+function display_usage {
+    echo "Usage: $0 [-l|--language python|julia] [-t|--type gpu|cpu] [-i|install] [-s|--student] [-b|--benchmark] [-h|--help]"
+    echo ""
+    echo "Description:"
+    echo "This script is designed to run either Python or Julia scripts for CPU or GPU, with optional benchmarking and student GPU partition."
+    echo "It provides flexibility in choosing the language, execution type, and other options."
+    echo ""
+    echo "Options:"
+    echo "-l, --language    Choose the scripting language (python or julia)."
+    echo "-t, --type        Specify the execution type (gpu or cpu)."
+    echo "-i, --install     Install the required packages before script execution (optional)."
+    echo "-s, --student     Use the student GPU partition (optional)."
+    echo "-b, --benchmark   Run benchmark after script execution (optional)."
+    echo "-h, --help        Display this usage message and exit."
+    echo ""
+    echo "Examples:"
+    echo "1. Run Python script on GPU partition:"
+    echo "   $0 -l python -t gpu"
+    echo ""
+    echo "2. Run Julia script on CPU:"
+    echo "   $0 -l julia -t cpu"
+    echo ""
+    echo "3. Run Python script on student GPU partition with benchmark:"
+    echo "   $0 -l python -t gpu -s -b"
+    echo ""
+    echo "4. Display script usage information:"
+    echo "   $0 -h"
+    exit 1
+}
+
 # Parse arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -40,6 +71,7 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
+
 
 
 # Run benchmark if requested
@@ -84,34 +116,3 @@ else
     echo "Invalid language: $language"
     display_usage
 fi
-
-# Function to display script usage
-function display_usage {
-    echo "Usage: $0 [-l|--language python|julia] [-t|--type gpu|cpu] [-i|install] [-s|--student] [-b|--benchmark] [-h|--help]"
-    echo ""
-    echo "Description:"
-    echo "This script is designed to run either Python or Julia scripts for CPU or GPU, with optional benchmarking and student GPU partition."
-    echo "It provides flexibility in choosing the language, execution type, and other options."
-    echo ""
-    echo "Options:"
-    echo "-l, --language    Choose the scripting language (python or julia)."
-    echo "-t, --type        Specify the execution type (gpu or cpu)."
-    echo "-i, --install     Install the required packages before script execution (optional)."
-    echo "-s, --student     Use the student GPU partition (optional)."
-    echo "-b, --benchmark   Run benchmark after script execution (optional)."
-    echo "-h, --help        Display this usage message and exit."
-    echo ""
-    echo "Examples:"
-    echo "1. Run Python script on GPU partition:"
-    echo "   $0 -l python -t gpu"
-    echo ""
-    echo "2. Run Julia script on CPU:"
-    echo "   $0 -l julia -t cpu"
-    echo ""
-    echo "3. Run Python script on student GPU partition with benchmark:"
-    echo "   $0 -l python -t gpu -s -b"
-    echo ""
-    echo "4. Display script usage information:"
-    echo "   $0 -h"
-    exit 1
-}
--- src/python-tensorflow/benchmark.py
+++ src/python-tensorflow/benchmark.py
@@ -1,98 +1,90 @@
+# -*- coding: utf-8 -*-
+
 import matplotlib.pyplot as plt
 import json
 
 CPU_JULIA = json.load(open("./json/CPU_JULIA_epoch_data.json"))
-GPU_JULIA = json.load(open("./json/GPU_JULIA_epoch_data.json"))
 CPU_PYTHON = json.load(open("./json/CPU_PYTHON_epoch_data.json"))
-GPU_PYTHON = json.load(open("./json/GPU_PYTHON_epoch_data.json"))
+SMALL_GPU_PYTHON = json.load(open("./json/SMALL_GPU_PYTHON_epoch_data.json"))
+SMALL_GPU_JULIA = json.load(open("./json/SMALL_GPU_JULIA_epoch_data.json"))
+MEDIUM_GPU_PYTHON = json.load(open("./json/MEDIUM_GPU_PYTHON_epoch_data.json"))
+MEDIUM_GPU_JULIA = json.load(open("./json/MEDIUM_GPU_JULIA_epoch_data.json"))
+BIG_GPU_PYTHON = json.load(open("./json/BIG_GPU_PYTHON_epoch_data.json"))
+BIG_GPU_JULIA = json.load(open("./json/BIG_GPU_JULIA_epoch_data.json"))
 
+print(CPU_PYTHON.keys())
+print(CPU_JULIA.keys())
+
+print(SMALL_GPU_PYTHON.keys())
+print(SMALL_GPU_JULIA.keys())
+
+print(MEDIUM_GPU_PYTHON.keys())
+print(MEDIUM_GPU_JULIA.keys())
+
+print(BIG_GPU_PYTHON.keys())
+print(BIG_GPU_JULIA.keys())
+
+"""# COMPARE PYTHON GPU CPU"""
 
 fig, axs = plt.subplots(2, 3, figsize=(15, 10))
 
-axs[0, 0].plot(GPU_PYTHON["epoch"], GPU_PYTHON["epoch_time"], label='epoch_time')
-axs[0, 0].plot(CPU_PYTHON["epoch"], CPU_PYTHON["epoch_time"], label='epoch_time')
+axs[0, 0].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["epoch_time"], label='GPU')
+axs[0, 0].plot(CPU_PYTHON["epoch"], CPU_PYTHON["epoch_time"], label='CPU')
 
 
-axs[0, 1].plot(GPU_PYTHON["epoch"], GPU_PYTHON["batch_processing_time"], label='batch_processing_time')
-axs[0, 1].plot(CPU_PYTHON["epoch"], CPU_PYTHON["batch_processing_time"], label='batch_processing_time')
+axs[0, 1].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["batch_processing_time"], label='GPU')
+axs[0, 1].plot(CPU_PYTHON["epoch"], CPU_PYTHON["batch_processing_time"], label='CPU')
 
 
-axs[0, 2].plot(GPU_PYTHON["epoch"], GPU_PYTHON["throughput"], label='throughput')
-axs[0, 2].plot(CPU_PYTHON["epoch"], CPU_PYTHON["throughput"], label='throughput')
+axs[0, 2].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["throughput"], label='GPU')
+axs[0, 2].plot(CPU_PYTHON["epoch"], CPU_PYTHON["throughput"], label='CPU')
 
 
-axs[1, 0].plot(GPU_PYTHON["epoch"], GPU_PYTHON["accuracy"], label='accuracy')
-axs[1, 0].plot(CPU_PYTHON["epoch"], CPU_PYTHON["accuracy"], label='accuracy')
+axs[1, 0].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["accuracy"], label='GPU')
+axs[1, 0].plot(CPU_PYTHON["epoch"], CPU_PYTHON["accuracy"], label='CPU')
 
 
-axs[1, 1].plot(GPU_PYTHON["epoch"], GPU_PYTHON["loss"], label='loss')
-axs[1, 1].plot(CPU_PYTHON["epoch"], CPU_PYTHON["loss"], label='loss')
+axs[1, 1].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["loss"], label='GPU')
+axs[1, 1].plot(CPU_PYTHON["epoch"], CPU_PYTHON["loss"], label='CPU')
 
-for ax, lab in zip(axs.flat, list(GPU_PYTHON.keys())[2:]):
+for ax, lab in zip(axs.flat, list(MEDIUM_GPU_PYTHON.keys())[2:]):
     ax.set(xlabel='Epoch', ylabel='Value')
     ax.legend()
 
 plt.tight_layout()
-plt.savefig("./image/python.png")
+plt.savefig("./image/PYTHON_GPU_CPU.png")
+
+"""# COMPARE JULIA CPU GPU"""
 
 fig, axs = plt.subplots(2, 3, figsize=(15, 10))
 
-axs[0, 0].plot(GPU_JULIA["epoch"], GPU_JULIA["epoch_time"], label='GPU')
+axs[0, 0].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["epoch_time"], label='GPU')
 axs[0, 0].plot(CPU_JULIA["epoch"], CPU_JULIA["epoch_time"], label='CPU')
 
-axs[0, 1].plot(GPU_JULIA["epoch"], GPU_JULIA["batch_processing_time"], label='GPU')
+
+axs[0, 1].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["batch_processing_time"], label='GPU')
 axs[0, 1].plot(CPU_JULIA["epoch"], CPU_JULIA["batch_processing_time"], label='CPU')
 
 
-axs[0, 2].plot(GPU_JULIA["epoch"], GPU_JULIA["throughput"], label='GPU')
+axs[0, 2].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["throughput"], label='GPU')
 axs[0, 2].plot(CPU_JULIA["epoch"], CPU_JULIA["throughput"], label='CPU')
 
 
-axs[1, 0].plot(GPU_JULIA["epoch"], GPU_JULIA["accuracy"], label='GPU')
+axs[1, 0].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["accuracy"], label='GPU')
 axs[1, 0].plot(CPU_JULIA["epoch"], CPU_JULIA["accuracy"], label='CPU')
 
 
-axs[1, 1].plot(GPU_JULIA["epoch"], GPU_JULIA["loss"], label='GPU')
+axs[1, 1].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["loss"], label='GPU')
 axs[1, 1].plot(CPU_JULIA["epoch"], CPU_JULIA["loss"], label='CPU')
 
-for ax, lab in zip(axs.flat, list(GPU_JULIA.keys())[2:]):
+for ax, lab in zip(axs.flat, list(MEDIUM_GPU_PYTHON.keys())[2:]):
     ax.set(xlabel='Epoch', ylabel=lab)
     ax.legend()
 
 plt.tight_layout()
-plt.savefig("./image/julia.png")
+plt.savefig("./image/JULIA_GPU_CPU.png")
 
-fig, axs = plt.subplots(2, 3, figsize=(15, 10))
-
-if "epoch_memory_usage" in GPU_JULIA.keys():
-    axs[0, 0].plot(GPU_JULIA["epoch"], GPU_JULIA["epoch_memory_usage"], label='JULIA')
-axs[0, 0].plot(GPU_PYTHON["epoch"], GPU_PYTHON["epoch_memory_usage"], label='PYTHON')
-
-axs[0, 1].plot(GPU_JULIA["epoch"], GPU_JULIA["epoch_time"], label='JULIA')
-axs[0, 1].plot(GPU_PYTHON["epoch"], GPU_PYTHON["epoch_time"], label='PYTHON')
-
-
-axs[0, 2].plot(GPU_JULIA["epoch"], GPU_JULIA["batch_processing_time"], label='JULIA')
-axs[0, 2].plot(GPU_PYTHON["epoch"], GPU_PYTHON["batch_processing_time"], label='PYTHON')
-
-
-axs[1, 0].plot(GPU_JULIA["epoch"], GPU_JULIA["throughput"], label='JULIA')
-axs[1, 0].plot(GPU_PYTHON["epoch"], GPU_PYTHON["throughput"], label='PYTHON')
-
-
-axs[1, 1].plot(GPU_JULIA["epoch"], GPU_JULIA["accuracy"], label='JULIA')
-axs[1, 1].plot(GPU_PYTHON["epoch"], GPU_PYTHON["accuracy"], label='PYTHON')
-
-
-axs[1, 2].plot(GPU_JULIA["epoch"], GPU_JULIA["loss"], label='JULIA')
-axs[1, 2].plot(GPU_PYTHON["epoch"], GPU_PYTHON["loss"], label='PYTHON')
-
-for ax, lab in zip(axs.flat, list(GPU_JULIA.keys())[1:]):
-    ax.set(xlabel='Epoch', ylabel=lab)
-    ax.legend()
-
-plt.tight_layout()
-plt.savefig("./image/julia_python_gpu.png")
+"""# COMPARE CPU"""
 
 fig, axs = plt.subplots(2, 3, figsize=(15, 10))
 
@@ -120,7 +112,196 @@ for ax, lab in zip(axs.flat, list(CPU_PYTHON.keys())[2:]):
     ax.legend()
 
 plt.tight_layout()
-plt.savefig("./image/julia_python_cpu.png")
+plt.savefig("./image/CPU.png")
+
+"""# COMPARE PYTHON  JULIA GPU
+
+# SMALL
+"""
+
+fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+
+axs[0, 0].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["epoch_memory_usage"], label='JULIA')
+axs[0, 0].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["epoch_memory_usage"], label='PYTHON')
+
+axs[0, 1].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["epoch_time"], label='JULIA')
+axs[0, 1].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["epoch_time"], label='PYTHON')
+
+
+axs[0, 2].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["batch_processing_time"], label='JULIA')
+axs[0, 2].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["batch_processing_time"], label='PYTHON')
+
+
+axs[1, 0].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["throughput"], label='JULIA')
+axs[1, 0].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["throughput"], label='PYTHON')
+
+
+axs[1, 1].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["accuracy"], label='JULIA')
+axs[1, 1].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["accuracy"], label='PYTHON')
+
+
+axs[1, 2].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["loss"], label='JULIA')
+axs[1, 2].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["loss"], label='PYTHON')
+
+for ax, lab in zip(axs.flat, list(SMALL_GPU_PYTHON.keys())[1:]):
+    ax.set(xlabel='Epoch', ylabel=lab)
+    ax.legend()
+
+plt.tight_layout()
+plt.savefig("./image/SMALL_GPU.png")
+
+"""# MEDIUM"""
+
+fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+
+axs[0, 0].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["epoch_memory_usage"], label='JULIA')
+axs[0, 0].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["epoch_memory_usage"], label='PYTHON')
+
+axs[0, 1].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["epoch_time"], label='JULIA')
+axs[0, 1].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["epoch_time"], label='PYTHON')
+
+
+axs[0, 2].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["batch_processing_time"], label='JULIA')
+axs[0, 2].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["batch_processing_time"], label='PYTHON')
+
+
+axs[1, 0].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["throughput"], label='JULIA')
+axs[1, 0].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["throughput"], label='PYTHON')
+
+
+axs[1, 1].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["accuracy"], label='JULIA')
+axs[1, 1].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["accuracy"], label='PYTHON')
+
+
+axs[1, 2].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["loss"], label='JULIA')
+axs[1, 2].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["loss"], label='PYTHON')
+
+for ax, lab in zip(axs.flat, list(MEDIUM_GPU_PYTHON.keys())[1:]):
+    ax.set(xlabel='Epoch', ylabel=lab)
+    ax.legend()
+
+plt.tight_layout()
+plt.savefig("./image/MEDIUM_GPU.png")
+
+"""# BIG"""
+
+fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+
+axs[0, 0].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["epoch_memory_usage"], label='JULIA')
+axs[0, 0].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["epoch_memory_usage"], label='PYTHON')
+
+axs[0, 1].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["epoch_time"], label='JULIA')
+axs[0, 1].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["epoch_time"], label='PYTHON')
+
+
+axs[0, 2].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["batch_processing_time"], label='JULIA')
+axs[0, 2].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["batch_processing_time"], label='PYTHON')
+
+
+axs[1, 0].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["throughput"], label='JULIA')
+axs[1, 0].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["throughput"], label='PYTHON')
+
+
+axs[1, 1].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["accuracy"], label='JULIA')
+axs[1, 1].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["accuracy"], label='PYTHON')
+
+
+axs[1, 2].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["loss"], label='JULIA')
+axs[1, 2].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["loss"], label='PYTHON')
+
+for ax, lab in zip(axs.flat, list(BIG_GPU_PYTHON.keys())[1:]):
+    ax.set(xlabel='Epoch', ylabel=lab)
+    ax.legend()
+
+plt.tight_layout()
+plt.savefig("./image/BIG_GPU.png")
+
+"""# VIEW EVOLUTION OF TIME WITH SIZE
+
+# Python
+"""
+
+fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+
+axs[0, 0].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["epoch_memory_usage"], label='SMALL')
+axs[0, 0].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["epoch_memory_usage"], label='MEDIUM')
+axs[0, 0].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["epoch_memory_usage"], label='BIG')
+
+axs[0, 1].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["epoch_time"], label='SMALL')
+axs[0, 1].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["epoch_time"], label='MEDIUM')
+axs[0, 1].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["epoch_time"], label='BIG')
+
+
+axs[0, 2].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["batch_processing_time"], label='SMALL')
+axs[0, 2].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["batch_processing_time"], label='MEDIUM')
+axs[0, 2].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["batch_processing_time"], label='BIG')
+
+
+axs[1, 0].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["throughput"], label='SMALL')
+axs[1, 0].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["throughput"], label='MEDIUM')
+axs[1, 0].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["throughput"], label='BIG')
+
+
+axs[1, 1].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["accuracy"], label='SMALL')
+axs[1, 1].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["accuracy"], label='MEDIUM')
+axs[1, 1].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["accuracy"], label='BIG')
+
+
+axs[1, 2].plot(SMALL_GPU_PYTHON["epoch"], SMALL_GPU_PYTHON["loss"], label='SMALL')
+axs[1, 2].plot(MEDIUM_GPU_PYTHON["epoch"], MEDIUM_GPU_PYTHON["loss"], label='MEDIUM')
+axs[1, 2].plot(BIG_GPU_PYTHON["epoch"], BIG_GPU_PYTHON["loss"], label='BIG')
+
+for ax, lab in zip(axs.flat, list(BIG_GPU_PYTHON.keys())[1:]):
+    ax.set(xlabel='Epoch', ylabel=lab)
+    ax.legend()
+
+plt.tight_layout()
+plt.savefig("./image/ALL_PYTHON.png")
+
+"""# JULIA"""
+
+fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+
+axs[0, 0].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["epoch_memory_usage"], label='SMALL')
+axs[0, 0].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["epoch_memory_usage"], label='MEDIUM')
+axs[0, 0].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["epoch_memory_usage"], label='BIG')
+
+axs[0, 1].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["epoch_time"], label='SMALL')
+axs[0, 1].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["epoch_time"], label='MEDIUM')
+axs[0, 1].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["epoch_time"], label='BIG')
+
+
+axs[0, 2].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["batch_processing_time"], label='SMALL')
+axs[0, 2].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["batch_processing_time"], label='MEDIUM')
+axs[0, 2].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["batch_processing_time"], label='BIG')
+
+
+axs[1, 0].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["throughput"], label='SMALL')
+axs[1, 0].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["throughput"], label='MEDIUM')
+axs[1, 0].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["throughput"], label='BIG')
+
+
+axs[1, 1].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["accuracy"], label='SMALL')
+axs[1, 1].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["accuracy"], label='MEDIUM')
+axs[1, 1].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["accuracy"], label='BIG')
+
+
+axs[1, 2].plot(SMALL_GPU_JULIA["epoch"], SMALL_GPU_JULIA["loss"], label='SMALL')
+axs[1, 2].plot(MEDIUM_GPU_JULIA["epoch"], MEDIUM_GPU_JULIA["loss"], label='MEDIUM')
+axs[1, 2].plot(BIG_GPU_JULIA["epoch"], BIG_GPU_JULIA["loss"], label='BIG')
+
+for ax, lab in zip(axs.flat, list(BIG_GPU_PYTHON.keys())[1:]):
+    ax.set(xlabel='Epoch', ylabel=lab)
+    ax.legend()
+
+plt.tight_layout()
+plt.savefig("./image/ALL_JULIA.png")
+
 
 print("Done!")
 print("Images saved in ./image/")
